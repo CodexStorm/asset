4ea07968ba67cbdbe281b913b2dd7c24bf5e4391
DESKTOP-PKFT7F6\Prabhu Sivanandam <prabhu231197@gmail.com>
2017-06-15 19:28:03 +0530

    Attendance coplete

--- app/src/main/java/com/ninja/ultron/adapter/AssetListRecyclerAdapter.java
+++ app/src/main/java/com/ninja/ultron/adapter/AssetListRecyclerAdapter.java
@@ -22,6 +22,7 @@ import java.util.List;
 public class AssetListRecyclerAdapter extends RecyclerView.Adapter<AssetListRecyclerAdapter.ViewHolder>{
 
     List<CodeDecodeEntity> assetsList=new ArrayList<>();
+    int c;
     Context context;
     private CallBack mCallBack;
 
